411fddfb5997e1b148700432afbc0bfdf8e846f0
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2021-10-02 21:21:59 +0200

    adds router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import { Header } from './components';
 import { MainView } from './vews';
 
 const App = () => (
   <div className="container mx-auto">
     <Header />
-    <MainView />
+    <Switch>
+      <Route path="/" exact component={MainView} />
+      <Redirect to="/" />
+    </Switch>
   </div>
 );
 
